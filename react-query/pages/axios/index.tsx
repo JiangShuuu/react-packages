e6616809c901047ecf7fb9e17d9f12e3a89e88ca
JiangShuuu <globelex65@gmail.com>
2022-11-18 11:59:53 +0800

    add

--- react-query/pages/axios/index.tsx
+++ react-query/pages/axios/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+const getData = async () => {
+  const { data } = await axios.get('https://swapi.dev/api/people/9');
+  return data;
+};
 
 export default function index() {
+  const { data, isLoading, isError } = useQuery(['static_axios'], getData);
+  console.log(data);
+
+  if (isLoading) {
+    return <h1>isLoading</h1>;
+  }
+
+  if (isError) {
+    return <h1>Error</h1>;
+  }
+
   return (
     <div>
       <main>
@@ -14,7 +32,7 @@ export default function index() {
           <Link href='/axios/ssg'>Axios SSG</Link>
         </nav>
         <h1>Axios Static Data</h1>
-        <div></div>
+        <div>{data.name}</div>
       </main>
     </div>
   );
