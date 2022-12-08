e73da5126ea29c8af3caacbfc775e02fe03e8873
JiangShuuu <globelex65@gmail.com>
2022-12-08 16:25:15 +0800

    upd

--- react-form/pages/form/yup.tsx
+++ react-form/pages/form/yup.tsx
@@ -1,36 +1,73 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-
+import i18n from 'i18next';
+import { useTranslation, initReactI18next } from 'react-i18next';
+import zh from '../../public/locales/zh-Hans/common.json';
+import hant from '../../public/locales/zh-Hant/common.json';
 interface IFormInputs {
   firstName: string;
   age: number;
 }
 
-const schema = yup.object({
-  firstName: yup.string().required('嗨嗨'),
-  age: yup.number().positive().integer().required()
-});
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources: {
+      en: {
+        translation: zh
+      },
+      hant: {
+        translation: hant
+      }
+    },
+    lng: 'en', // if you're using a language detector, do not define the lng option
+    fallbackLng: 'en',
+
+    interpolation: {
+      escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    }
+  });
 
 export default function App() {
+  const { t } = useTranslation();
+  const { changeLanguage, language } = i18n;
+
+  console.log(language);
+
+  const firstN = t('不可空白', { hant });
+
+  const schemaI18n = yup.object({
+    firstName: yup.string().required(firstN)
+  });
+
+  const changTest = language === 'en' ? 'hant' : 'en';
+  // const change = i18n.changeLanguage('hant');
+
   const {
     register,
     handleSubmit,
     formState: { errors }
   } = useForm<IFormInputs>({
-    resolver: yupResolver(schema)
+    resolver: yupResolver(schemaI18n)
   });
   const onSubmit = (data: IFormInputs) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('firstName')} />
-      <p>{errors.firstName?.message}</p>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('firstName')} />
+        <p>{errors.firstName?.message}</p>
 
-      <input {...register('age')} />
-      <p>{errors.age?.message}</p>
+        <input {...register('age')} />
+        <p>{errors.age?.message}</p>
 
-      <input type='submit' />
-    </form>
+        <input type='submit' />
+      </form>
+      <button onClick={() => changeLanguage(changTest)}>案我</button>
+    </>
   );
 }
