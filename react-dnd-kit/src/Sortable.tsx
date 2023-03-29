4f09f3432f68be345390498fc95108f2476f9912
JiangShuuu <globelex65@gmail.com>
2023-03-29 21:48:59 +0800

    upd

--- react-dnd-kit/src/Sortable.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Sortable() {
-  return <div>Sortable</div>
-}
---
+++ react-dnd-kit/src/pages/index.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { createRange } from '../../utilities'
+import SortableList from '../../components/SortableList'
+
+function getMockItems() {
+  return createRange(50, (index) => ({ id: index + 1 }))
+}
+
+export default function Index() {
+  const [items, setItems] = useState(getMockItems)
+
+  return (
+    <div style={{ maxWidth: 400, margin: '30px auto' }}>
+      <SortableList
+        items={items}
+        onChange={setItems}
+        renderItem={(item) => (
+          <SortableList.Item id={item.id}>
+            {item.id}
+            <SortableList.DragHandle />
+          </SortableList.Item>
+        )}
+      />
+    </div>
+  )
+}
