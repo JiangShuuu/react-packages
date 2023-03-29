e087e34db18abb82c88de802252354cef8880e48
JiangShuuu <globelex65@gmail.com>
2023-03-29 22:01:07 +0800

    upd

--- react-dnd-kit/src/App.tsx
+++ react-dnd-kit/src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Index from './pages/index'
+import Grid from './pages/grid'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />}></Route>
-        {/* <Route path="/02" element={<Index02 />}></Route>
-        <Route path="/03" element={<Index03 />}></Route>
+        <Route path="/grid" element={<Grid />}></Route>
+        {/* <Route path="/03" element={<Index03 />}></Route>
         <Route path="/04" element={<Index04 />}></Route> */}
       </Routes>
     </BrowserRouter>
